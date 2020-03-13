7df26ef931074c22fe77bb61f0e2fe67d974c6b9
songp2018 <35275347+songp2018@users.noreply.github.com>
2020-03-13 19:01:42 -0400

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Application.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public String Index()//ActionResult-> string
+        {
+            return "Product from Index";//View()-> "Product"
+        }
+
+        // GET: Product/Browse
+        public String Browse()//added
+        {
+            return "Browse displayed";
+        }
+
+        // GET: Product/Details/105
+        public String Details(int id) //added
+        {
+            string message = "Details dilplayed for Id = " + id;
+            return message;
+        }
+
+        // GET: Product/Location?zip=44124
+        public String Location(int zip) //added
+        {
+            return "Location displayed for zip= "+ zip;
+        }
+    }
+}
